3fcd9718910387895e515318cab4dc55d7d0e510
Jeremy Walton <jthelegoman@gmail.com>
2016-07-18 16:59:03 -0400

    Upgrade to use ES6 and clean up syntax

--- youtube_watch_later_time_calculator/youtube_watch_later_time_calculator.js
+++ youtube_watch_later_time_calculator/youtube_watch_later_time_calculator.js
@@ -1,45 +1,37 @@
-var modulus = function(n, m) {
-  return ((n % m) + m) % m;
+var modulus = (n, m) => ((n % m) + m) % m;
+var parsedTime = time => time < 10 ? '0' + time : time;
+
+var calculateTime = array => {
+  const totalTime = array.reduce(function(runningTotal, div) {
+    const parts = div.textContent.split(':').map(part => parseFloat(part));
+    if (parts.length < 3) { parts.unshift(0) }
+    return runningTotal + (parts[0] * 3600) + (parts[1] * 60) + parts[2];
+  }, 0);
+
+  const hours = modulus(parseInt(totalTime / 3600), 24);
+  const minutes = modulus(parseInt(totalTime / 60), 60);
+  const seconds = modulus(totalTime, 60);
+  return ` ${parsedTime(hours)}:${parsedTime(minutes)}:${parsedTime(seconds)}`;
 };
 
-var timer = function() {
-  tag.innerText = tick + ' ' + baseText;
+var timer = () => {
+  tag.innerText = `${tick} ${baseText}`;
   tick--;
-
   if (tick > -1) {
     setTimeout(timer, 1000);
   }
 };
 
-var stamps = Array.prototype.slice.call(document.getElementsByClassName('timestamp'));
-var zeroDate = d2 = new Date('Jun 22 2016 00:00:00');
-
-var sum = stamps.map(function(stamp) {
-  return stamp.children[0].innerText;
-}).map(function(time) {
-  if (time.length < 6) {
-    time = '00:' + time;
-  }
-  var date = new Date('Jun 22 2016 ' + time);
-  return (date.getTime() - zeroDate.getTime()) / 1000;
-}).reduce(function(a, b) {
-  return a + b;
-}, 0);
-
-var hours = modulus(parseInt(sum / 3600), 24);
-var minutes = modulus(parseInt(sum / 60), 60);
-var seconds = modulus(sum, 60);
-var result = (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds  < 10 ? '0' + seconds : seconds);
-
+var divs = Array.from(document.getElementsByClassName('timestamp'));
+var baseText = ` Total Playlist Time: ${calculateTime(divs)}`;
 var tag = document.createElement('p');
-var baseText = 'Total Playlist time: ' + result;
 tag.innerText = baseText;
 tag.style.cssText = 'position: fixed; left: 50%; top: 10px; z-index: 3000000000; padding: 10px; background-color: gray;';
 document.body.appendChild(tag);
 
-setTimeout(function() {
+var tick = 5;
+setTimeout(() => {
   document.body.removeChild(tag);
-}, 10000);
+}, tick * 1000);
 
-var tick = 10;
 timer();
